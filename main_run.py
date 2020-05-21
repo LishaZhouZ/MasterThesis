69296c89f1b7bdddda75201f73f097ecaa07d078
Zhou <ge29nab@tueilmt-gpuserv.lmt.ei.tum.de>
2020-05-21 23:10:08 +0200

    change main data folder

--- main_run.py
+++ main_run.py
@@ -20,18 +20,17 @@ if __name__ == '__main__':
     if gpus:
       try:
       # Currently, memory growth needs to be the same across GPUs
-        for gpu in gpus:
-          tf.config.experimental.set_visible_devices(gpus[1], 'GPU')
-          logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-          print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
+        tf.config.experimental.set_visible_devices(gpus[3], 'GPU')
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+      except RuntimeError as e:
       # Memory growth must be set before GPUs have been initialized
           print(e)
 
 
     #read dataset
-    train_dataset = read_and_decode('./patches/MWCNN_train_data_debug.tfrecords')
-    val_dataset = read_and_decode('./patches/MWCNN_validation_data.tfrecords')
+    train_dataset = read_and_decode('/mnt/data4/Student/Lisha/patches/MWCNN_train_data_debug.tfrecords')
+    val_dataset = read_and_decode('/mnt/data4/Student/Lisha/patches/MWCNN_validation_data.tfrecords')
     #build model
     model = DnCNN()
 
